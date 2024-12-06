f2111ccc220d493f99834fc3568ba47df3ed777d
Darrellson <muraddiasamidze86@gmail.com>
2024-12-06 15:32:34 +0400

    1

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/main.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/main.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+export {};
