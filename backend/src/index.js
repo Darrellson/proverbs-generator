cb0e3a64eeec12d4e6f73b3e8f4ca15999ab6c1f
Darrellson <muraddiasamidze86@gmail.com>
2024-12-06 15:22:16 +0400

    1

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,3 +16,12 @@ app.use('/proverbs', proverbRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "proverbs-generator.vercel.app", // Replace with your Vercel domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // If using cookies or auth headers
+  })
+);
