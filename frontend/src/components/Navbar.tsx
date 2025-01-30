a86efd58f20df28aa125edbd0b59454b3cbcfd08
Darrellson <muraddiasamidze86@gmail.com>
2025-01-31 02:16:59 +0400

    fit:home

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <Link className="navbar-brand" to="/">Proverbs App</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            {/* <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li> */}
             <li className="nav-item"><Link className="nav-link" to="/proverbs">Proverbs</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/admin">Admin Panel</Link></li>
           </ul>
